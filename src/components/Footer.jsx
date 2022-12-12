8c95d7cc79f6ee0c4f2873a5fe33bbe577e507b8
KvinPardo <kevin.pardov25@gmail.com>
2022-12-11 23:28:29 -0300

    PreEntrega2 lista

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,69 +7,70 @@ import {
   FaFacebookSquare,
   FaInstagram,
   FaTwitterSquare,
-  FaWhatsappSquare
+  FaWhatsappSquare,
 } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="bg-[#f1eae4]">
-      <div className="container mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300 ">
+      <div className="container mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
         <div className="text-primary">
           <h1 className="w-full text-3xl font-bold">Pía Bordados</h1>
           <p className="py-4 text-primary">
-            Somos una pequeña familia que se atrevió a emprender en el rubro de los bordados.
+            Somos una pequeña familia que se atrevió a emprender en el rubro de
+            los bordados.
           </p>
           <div className="flex justify-between md:w-[75%] my-6">
-           <a href="https://www.facebook.com/PuraArtesanos?mibextid=LQQJ4d" target="_blank"><FaFacebookSquare size={30}/></a>
-            <FaInstagram size={30} />
-            <FaTwitterSquare size={30} />
-            <FaWhatsappSquare  size={30}/>
-            
+            <a href="/" target="_blank">
+              <FaFacebookSquare size={30} />
+            </a>
+            <a href="/" target="_blank">
+              <FaInstagram size={30} />
+            </a>
+            <a href="/" target="_blank">
+              <FaTwitterSquare size={30} />
+            </a>
+            <a href="/" target="_blank">
+              <FaWhatsappSquare size={30} />
+            </a>
           </div>
         </div>
         <div className="lg:col-span-2 flex justify-between mt-6">
           <div>
-            <h6 className="font-medium text-primary">Solutions</h6>
+            <h6 className="font-medium text-primary">Ayuda</h6>
             <ul>
-              <li className="py-2 text-sm p-0">Analytics</li>
-              <li className="py-2 text-sm p-0">Marketing</li>
-              <li className="py-2 text-sm p-0">Commerce</li>
-              <li className="py-2 text-sm p-0">Insights</li>
+              <li className="py-2 text-sm p-0">¿Cómo comprar?</li>
+              <li className="py-2 text-sm p-0">Devoluciones</li>
+              <li className="py-2 text-sm p-0">Métodos de pago</li>
+              <li className="py-2 text-sm p-0">Envío a Regiones</li>
             </ul>
           </div>
           <div>
-            <h6 className="font-medium text-primary">Support</h6>
+            <h6 className="font-medium text-primary"></h6>
             <ul>
-              <li className="py-2 text-sm p-0">Pricing</li>
-              <li className="py-2 text-sm p-0">Documentation</li>
-              <li className="py-2 text-sm p-0">Guides</li>
-              <li className="py-2 text-sm p-0">API Status</li>
-            </ul>
-          </div>
-          <div>
-            <h6 className="font-medium text-primary">Company</h6>
-            <ul>
-              <li className="py-2 text-sm p-0">About</li>
               <li className="py-2 text-sm p-0">Blog</li>
-              <li className="py-2 text-sm p-0">Jobs</li>
-              <li className="py-2 text-sm p-0">Press</li>
-              <li className="py-2 text-sm p-0">Careers</li>
+              <li className="py-2 text-sm p-0">Nuestros Clientes</li>
+              <li className="py-2 text-sm p-0">Videos Bordados</li>
+              <li className="py-2 text-sm p-0">Galería</li>
             </ul>
           </div>
-          <div>
-            <h6 className="font-medium text-primary">Legal</h6>
-            <ul>
-              <li className="py-2 text-sm">Claim</li>
-              <li className="py-2 text-sm">Policy</li>
-              <li className="py-2 text-sm">Terms</li>
+          <div className="flex flex-col">
+            <h6 className="font-medium text-primary">Pía</h6>
+            <ul className="flex flex-col text-primary">
+              <Link to={`/`} className="py-1">Inicio</Link>
+              <Link to={`/catalogo`} className="py-1">Catálogo</Link>
+              <Link to={`/servicios`} className="py-1">Servicios</Link>
+              <Link to={`/contacto`} className="py-1">Contacto</Link>
             </ul>
           </div>
         </div>
       </div>
+      <div className="text-center bg-[#f1eae2] h-full w-full">
+        &copy; Pía Bordados | Todos los Derechos Reservados
+      </div>
+      ;
     </footer>
   );
 };
 
 export default Footer;
-
-<div>&copy; Pía Bordados | Todos los Derechos Reservados</div>;
